85fb41183ab03059c781171933cdef38d4f07ed2
scoutchorton <scoutchorton@users.noreply.github.com>
2017-11-08 23:30:16 -0500

    Create sets.js

---
+++ sets.js
@@ -0,0 +1,17 @@
+var set1 = {
+"Factious": "Causing disagreements",
+"Ignoble": "Dishonorable or shameful",
+"Boor": "Rood/impolite person",
+"Aegis": "A shield or protection",
+"Perspicacity": "Keenness or sharpness of judgement",
+"Fervent": "Eager or earnest",
+"Rectify": "Correct",
+"Enervate": "To weaken",
+"Besiege": "To overwhelm, or to surround and attack",
+"Ephemeral": "Lasting only a brief time, or short lived",
+"Altruism": "A concern for others",
+"Carrion": "Decaying flesh",
+"Erotic": "Pertaining to sexual love",
+"Amorphous": "Shapeless, formless, or vague",
+"Opulent": "Rich, luxurious, wealthy"
+};
