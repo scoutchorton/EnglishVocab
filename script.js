7ad1c5e09efd29232565c3ee110e8e0f97508e4e
scoutchorton <scoutchorton@users.noreply.github.com>
2017-11-09 13:54:21 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
-fucntion revealSet1() {
+fucntion revealSet1(vocabSet) {
   var text = "";
   for(x in set1) {
-    text = text + x + ": "
+    text = text + x + ": " + set1[x] + "<br />";
   }
+  document.getElementbyId("words").innerHTML = vocabSet;
 };
